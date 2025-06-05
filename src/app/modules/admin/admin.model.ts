24caf0c7136e8798b535812d5d8271c4ce352005
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-05 15:41:08 +0600

    admin model  🐸

--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 import { AdminModel, IAdmin } from './admin.interface';
-
+// admin model
 const AdminSchema = new Schema<IAdmin, AdminModel>(
   {
     id: {
