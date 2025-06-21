c4252b5ad7e98278608824892f6bfd67bfda11d4
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-21 15:57:21 +0600

    Auth Route  🍯

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,7 +5,7 @@ import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
 const router = express.Router();
-
+// Auth Route
 router.post(
   '/login',
   validateRequest(AuthValidation.loginZodSchema),
